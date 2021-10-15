ff51f2e0637afed0528291633379d73c5f213a77
uc-nfdz <noe.fernandez@usercentrics.com>
2021-10-15 13:58:01 +0200

    CI WIP

--- scripts/setup_gradle.sh
+++ scripts/setup_gradle.sh
@@ -7,9 +7,11 @@ cd ci-cache/gradle
 
 curl -L -s "https://services.gradle.org/distributions/gradle-$GRADLE_VERSION-bin.zip" \
      --output "gradle-$GRADLE_VERSION-bin.zip"
-unzip gradle-6.7-bin.zip
+unzip -qq gradle-6.7-bin.zip
 rm gradle-6.7-bin.zip
 
 cd ../../
 
-./ci-cache/gradle/gradle-6.7/bin/gradle wrapper --gradle-version 6.7 --distribution-type all
+export PATH="$(pwd)/ci-cache/gradle/gradle-6.7/bin/gradle:$PATH"
+
+gradle wrapper --gradle-version 6.7 --distribution-type all
