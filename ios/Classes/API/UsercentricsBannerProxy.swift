e011df463a1965eb8e7b5908f3a5d40cf3f34eaf
Alexander Köhn <alibasta@gmail.com>
2022-05-04 16:31:02 +0200

    Updated method to access rootViewController
    Accessing the rootViewController using SceneDelegates inside the project is only possible this way.

--- ios/Classes/API/UsercentricsBannerProxy.swift
+++ ios/Classes/API/UsercentricsBannerProxy.swift
@@ -16,7 +16,7 @@ struct UsercentricsBannerProxy: UsercentricsBannerProxyProtocol {
     func showFirstLayer(bannerSettings: BannerSettings?,
                         layout: UsercentricsLayout,
                         completionHandler: @escaping (UsercentricsConsentUserResponse) -> Void) {
-        guard let flutterVC = UIApplication.shared.delegate?.window??.rootViewController as? FlutterViewController else { return }
+        guard let flutterVC = UIApplication.shared.windows.last?.rootViewController as? FlutterViewController else { return }
 
         UsercentricsBanner(bannerSettings: bannerSettings).showFirstLayer(hostView: flutterVC,
                                                                          layout: layout) { response in
