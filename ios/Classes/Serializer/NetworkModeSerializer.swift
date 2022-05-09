92ceb12d73b4e92e11f9a40f0c45bd365418d78f
uc-nfdz <noe.fernandez@usercentrics.com>
2022-05-09 18:30:52 +0100

    WIP

---
+++ ios/Classes/Serializer/NetworkModeSerializer.swift
@@ -0,0 +1,16 @@
+import Usercentrics
+
+extension NetworkMode {
+    static func initialize(from value: Any) -> NetworkMode? {
+        let valueString = value as! String
+        switch valueString {
+        case "WORLD":
+            return NetworkMode.world
+        case "EU":
+            return NetworkMode.eu
+        default:
+            assert(false)
+            return nil
+        }
+    }
+}
