ea189eaa7a5923dc5810342ca946698cb7f2c688
Francisco Cunha <francisco.cunha@usercentrics.com>
2024-03-07 16:59:17 +0000

    ready status serializer fix

--- lib/src/internal/serializer/ready_status_serializer.dart
+++ lib/src/internal/serializer/ready_status_serializer.dart
@@ -15,8 +15,10 @@ class ReadyStatusSerializer {
 }
 
 class GeolocationRulesetSerializer {
-  static GeolocationRuleset deserialize(dynamic value) {
-    return GeolocationRuleset(
+  static GeolocationRuleset? deserialize(dynamic value) {
+    return value == null
+        ? null
+        : GeolocationRuleset(
         activeSettingsId: value["activeSettingsId"],
         bannerRequiredAtLocation: value["bannerRequiredAtLocation"]);
   }
