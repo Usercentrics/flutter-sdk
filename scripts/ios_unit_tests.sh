63c2a52112004b2089fad1fa81d6341f1a41161f
andrei.smirennikov <andrei.smirennikov@quadcode.com>
2025-12-09 20:58:46 +0000

    Add code coverage

--- scripts/ios_unit_tests.sh
+++ scripts/ios_unit_tests.sh
@@ -4,6 +4,6 @@ rm -rf TestResults.xcresult
 
 xcodebuild test -workspace 'Runner.xcworkspace' \
                 -scheme 'Runner' \
-                -destination 'platform=iOS Simulator,name=iPhone 16 Pro' \
+                -destination 'platform=iOS Simulator,name=iPhone 15 Pro' \
                 -enableCodeCoverage YES \
                 -resultBundlePath TestResults
